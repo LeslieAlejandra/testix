32a93a1834cadbef3f991fd048600e5e84975afa
LESLIE ALEJANDRA BUSTOS <m20ce046@cenidet.tecnm.mx>
2021-09-28 12:59:44 -0500

    v2

--- server.js
+++ server.js
@@ -5,10 +5,10 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static('./dist/test-ix'));
+app.use(express.static('./dist/TestIX'));
 
 app.get('/*', (req, res) =>
-  res.sendFile('index.html', { root: 'dist/test-ix/' }),
+  res.sendFile('index.html', { root: 'dist/TestIX/' }),
 );
 
 // Start the app by listening on the default Heroku port
